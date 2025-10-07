c7804d8826a47575b5b89a6619e639dd5fe1422a
andycann44 <andycann44@yahoo.com>
2025-10-07 13:53:44 +0100

    Add: Local namespace shim for WorkbenchWindow

---
+++ Assets/AIGG/Core/LocalStubs.cs
@@ -0,0 +1,8 @@
+namespace Aim2Pro.AIGG.Local
+{
+    // Minimal shim so WorkbenchWindow compiles.
+    public static class NL
+    {
+        public static string Normalize(string s) => s;
+    }
+}
